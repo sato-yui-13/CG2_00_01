1617c2e369357557b6096f7a14a6e48cd64fa339
sato-yui-13 <k024g0002@g.neec.ac.jp>
2025-04-15 11:04:20 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<Windows.h>
+
+//Windowsアプリのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!\n");
+
+	return 0;
+}
