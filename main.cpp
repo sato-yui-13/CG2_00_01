60b8497350b57a2720ef96830ca27e1cdbd62004
sato-yui-13 <k024g0002@g.neec.ac.jp>
2025-04-22 11:30:50 +0900

    00_04

--- main.cpp
+++ main.cpp
@@ -4,13 +4,27 @@
 #include<format>
 
 
+// ウィンドウプロシージャ
+LRESULT CALLBACK WindoeProc(HWND hwnd, UINT msg, WPARAM wpaaram, LPARAM lparam) {
+	//メッセージに大路手ゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
 
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wpaaram, lparam);
+
+}
 
 //ウィンドウに文字を出す関数
 void Log(const std::string& message) {
 	OutputDebugStringA(message.c_str());
 }
-
 
 
 std::wstring ConvertString(const std::string& str) {
@@ -36,36 +50,24 @@ std::string ConvertString(const std::wstring& str) {
 	if (sizeNeeded == 0) {
 		return std::string();
 	}
-
 	std::string result(sizeNeeded, 0);
 	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
-
-
+	return result;
 }
 
 
-
-//ウィンドウプロシージャ
-LRESULT CALLBACK WindoeProc(HWND hwnd, UINT msg, WPARAM wpaaram, LPARAM lparam) {
-	//メッセージに大路手ゲーム固有の処理を行う
-	switch (msg) {
-		//ウィンドウが破棄された
-	case WM_DESTROY:
-		//OSに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-	}
-
-
-	//標準のメッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wpaaram, lparam);
-
-}
 
 //Windowsアプリのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	
+
 	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
+
+	//wstring->string
+	Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
+
 
 	WNDCLASS wc{};
 
@@ -101,19 +103,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		WS_OVERLAPPEDWINDOW,//よく見るウィンドウスタイル
 		CW_USEDEFAULT,      //表示X座標(Windowsに任せる)
 		CW_USEDEFAULT,      //表示Y座標(WindowsOSに任せる)
-		wrc.right-wrc.left, //ウィンドウ横幅
-		wrc.bottom-wrc.top, //ウィンドウ縦幅
+		wrc.right - wrc.left, //ウィンドウ横幅
+		wrc.bottom - wrc.top, //ウィンドウ縦幅
 		nullptr,            //親ウィンドウハンドル
 		nullptr,            //メニューハンドル
 		wc.hInstance,       //インスタンスハンドル
 		nullptr);           //オプション
 
 	//ウィンドウを表示する
-	ShowWindow(hwnd,SW_SHOW);
+	ShowWindow(hwnd, SW_SHOW);
 
 	MSG msg{};
-    //ウィンドウの×ボタンが押されるまでループ
-	while (msg.message!=WM_QUIT) {
+	//ウィンドウの×ボタンが押されるまでループ
+	while (msg.message != WM_QUIT) {
 		//Windowsにメッセージが来たら最優先で処理させる
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 
@@ -124,9 +126,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 	}
-	//wstring->string
-	Log(ConvertString(std::format(L"WSTRING{}\n",L"abc")));
 
 
-		return 0;
+
+	return 0;
 }
